f8db1b5f649ff7858dc32fb66c43311fcbef0964
Pedro Henrique Silva <146776908+Pedrohcsilva@users.noreply.github.com>
2024-09-09 16:22:54 -0300

    Add files via upload

---
+++ NumComplexo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    public class NumComplexo
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+
+        public NumComplexo()
+        {
+            Re = 0;
+            Im = 0;
+        }
+
+        public NumComplexo(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        public NumComplexo Soma(NumComplexo outro)
+        {
+            return new NumComplexo(Re + outro.Re, Im + outro.Im);
+        }
+
+        public NumComplexo Vezes(NumComplexo outro)
+        {
+            return new NumComplexo(Re * outro.Re - Im * outro.Im, Re * outro.Im + Im * outro.Re);
+        }
+
+        public double Modulo()
+        {
+            return Math.Sqrt(Re * Re + Im * Im);
+        }
+
+        public double Argumento()
+        {
+            return Math.Atan2(Im, Re);
+        }
+
+        public void ImprimeFormaPolar()
+        {
+            double modulo = Modulo();
+            double argumento = Argumento();
+            Console.WriteLine($"({modulo} * (cos({argumento}) + i * sin({argumento})))");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ComplexNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor real: ");
+            double re1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Entre com o valor imaginário: ");
+            double im1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Entre com o valor real do segundo: ");
+            double re2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Entre com o valor imaginário do segundo: ");
+            double im2 = Convert.ToDouble(Console.ReadLine());
+
+            NumComplexo z1 = new NumComplexo(re1, im1);
+            NumComplexo z2 = new NumComplexo(re2, im2);
+
+            NumComplexo soma = z1.Soma(z2);
+            NumComplexo produto = z1.Vezes(z2);
+
+            Console.WriteLine("Forma polar do complexo pela soma:");
+            soma.ImprimeFormaPolar();
+
+            Console.WriteLine("Forma polar do complexo pelo produto:");
+            produto.ImprimeFormaPolar();
+
+            Console.ReadLine();
+        }
+    }
+    }
